d017f5dd0bff16981f3cf483b49bcf292c996591
Noorulharisha <noorulharisha.n@xenovex.com>
2021-03-08 14:02:48 +0530

    signal strength view

--- app/src/main/java/com/example/sdklibraryapplication/MainActivity.java
+++ app/src/main/java/com/example/sdklibraryapplication/MainActivity.java
@@ -5,8 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.carlosmuvi.segmentedprogressbar.SegmentedProgressBar;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
